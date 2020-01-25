8b6d54ae597d2ae390cef9fd8f11e78a963964bd
arturolei <artie.lei@gmail.com>
2020-01-25 12:09:58 -0500

    Tested and finished oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -21,4 +21,20 @@
 class Vehicle: #this is the base class
     pass
 
+class FlightVehicle(Vehicle):
+    pass 
 
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass 
+
+class Motorcycle(GroundVehicle):
+    pass
