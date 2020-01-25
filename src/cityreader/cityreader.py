d2cec7fe5a46bf4cff8beb790900757b2d19488d
arturolei <artie.lei@gmail.com>
2020-01-25 12:30:04 -0500

    Working on cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,13 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
+  def __str__(self): #Did this so it would print nicely in print statement
+    return f"({self.name}, {self.lat}, {self.lon})"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -14,13 +21,21 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+
+import csv
 cities = []
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
+  # Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  with open('cities.csv') as cities_csv: #program must be run while in cityreader directory
+    csv_reader = csv.reader(cities_csv)
+    next(csv_reader) # next(variable name) --> move on fromo top row (which contains "column" names)
+    for row in csv_reader:
+      cities.append(City(row[0], float(row[3]), float(row[4]))) 
+      #float because of non-integer data
+      #row[x] where x = correct column
     return cities
 
 cityreader(cities)
